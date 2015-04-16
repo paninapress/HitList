a154429b368ea6c9a5f2e5520b72ad3a9f605b87
paninapress <npacifico25@gmail.com>
2015-04-16 01:00:52 -0700

    show/edit contact

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -3,10 +3,30 @@ class ConnectionsController < ApplicationController
 	def index
 		@connections = Connection.all
 	end
-    
+
 	def create
-        Connection.create!(first_name: params[:contact][:first_name], last_name: params[:contact][:last_name], fullname: params[:contact][:first_name]+ " " + params[:contact][:last_name])
+        new_contact = params.require(:connection).permit(:first_name, :last_name)
+        new_contact[:fullname] = params[:connection][:first_name]+ " " + params[:connection][:last_name]
+        Connection.create!(new_contact)
+
         redirect_to "/connections"
 	end
 
+    def show
+        @connection = Connection.find(params[:id])
+    end
+
+    def edit
+        @connection = Connection.find(params[:id])
+    end
+
+    def update
+        contact = Connection.find(params[:id])
+        updated_info = params.require(:connection).permit(:first_name, :last_name)
+        updated_info[:fullname] = params[:connection][:first_name]+ " " + params[:connection][:last_name]
+        contact.update_attributes(updated_info)
+
+        redirect_to connection_path(contact)
+    end
+    
 end
