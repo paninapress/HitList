153657a0f664654abc0b24326d8c3cd53045ca99
paninapress <npacifico25@gmail.com>
2015-12-15 17:37:16 -0800

    inital user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe User, :type => :model do
+    it "must have a name" do
+        user = FactoryGirl.build(:user, name: nil)
+        expect(user).to_not be_valid
+    end
+    it "must have an email" do
+    	user = FactoryGirl.build(:user, email: nil)
+    	expect(user).to_not be_valid
+    end
+    it "must have a password" do
+    	user = FactoryGirl.build(:user, password: nil)
+    	expect(user).to_not be_valid
+    end
+    it "must have a unique email" do
+    	user1 = FactoryGirl.create(:user, email: "test@test.com")
+    	expect(FactoryGirl.build(:user, email: "test@test.com")).to_not be_valid
+    end
+end
