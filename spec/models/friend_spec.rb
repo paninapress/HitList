35f942b8c86f02e32b2fd28fdc04506a9ab72181
paninapress <npacifico25@gmail.com>
2015-12-16 13:58:32 -0800

    initial friend specs

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Friend, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+    it "must have a name" do
+        friend = FactoryGirl.build(:friend, name: nil)
+        expect(friend).to_not be_valid
+    end
+    it "must have a name with less than 50 char" do
+        friend = FactoryGirl.build(:friend,name:"abcde"*12)
+        expect(friend).to_not be_valid
+    end
+    it "must have a category" do
+        friend = FactoryGirl.build(:friend, category: nil)
+        expect(friend).to_not be_valid
+    end
+    it "belongs to a user" do
+        friend = FactoryGirl.build(:friend, user_id: nil)
+        expect(friend).to_not be_valid
+    end
 end
