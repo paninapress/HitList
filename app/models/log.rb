ab913ed725b9cd1b8cb2064a466a2f50e22ffe75
paninapress <npacifico25@gmail.com>
2016-01-19 18:55:46 -0800

    filling in some pending log_specs

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,5 @@
 class Log < ActiveRecord::Base
   belongs_to :friend, dependent: :destroy
+  validates :date, presence: true
+  validates :friend_id, presence: true
 end
--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
 
 describe Log, :type => :model do
-	it "belongs to a Friend"
-	it "must have a date"
-	it "is valid without a type"
-	it "only allows Text, Audio, Video, In-Person responses for type"
-	it "is valid without a rating scale"
-	it "only allows the rating scale to be a number from 1-5"
-	it "is valid without a comment"
+    it "has a valid factory" do
+        log = FactoryGirl.build(:log)
+        expect(log).to be_valid
+    end
+    it "must have a date" do
+        log = FactoryGirl.build(:log, date: nil)
+        expect(log).to_not be_valid
+    end
+    it "belongs to a Friend" do
+        log = FactoryGirl.build(:log, friend_id: nil)
+        expect(log).to_not be_valid
+    end
+    it "is valid without a type" do
+        log = FactoryGirl.build(:log, type: nil)
+        expect(log).to be_valid
+    end
+    it "is valid without a rating scale" do
+        log = FactoryGirl.build(:log, rating: nil)
+        expect(log).to be_valid
+    end
+    it "is valid without a comment" do
+        log = FactoryGirl.build(:log, comment: nil)
+        expect(log).to be_valid
+    end
+    it "only allows Text, Audio, Video, In-Person responses for type"
+    it "only allows the rating scale to be a number from 1-5"
 end
