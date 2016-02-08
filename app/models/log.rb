7c8670ef59166adef4e63a3a417ae85fdc89e4ae
paninapress <npacifico25@gmail.com>
2016-02-08 15:58:10 -0800

    added log_specs for the rating scale

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,4 +2,7 @@ class Log < ActiveRecord::Base
   belongs_to :friend, dependent: :destroy
   validates :date, presence: true
   validates :friend_id, presence: true
+  validates :rating, numericality: {    only_integer: true, 
+                                        greater_than: 0, 
+                                        less_than_or_equal_to: 5 }, if: 'rating.present?'
 end
--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -17,14 +17,24 @@ describe Log, :type => :model do
         log = FactoryGirl.build(:log, type: nil)
         expect(log).to be_valid
     end
-    it "is valid without a rating scale" do
+    it "is valid without a rating" do
         log = FactoryGirl.build(:log, rating: nil)
         expect(log).to be_valid
+    end
+    it "has only allows integers for the rating" do
+        log = FactoryGirl.build(:log, rating: "something else")
+        expect(log).to_not be_valid
+        log2 = FactoryGirl.build(:log, rating: 1.435)
+        expect(log2).to_not be_valid
+    end
+    it "only allows the rating to be a number from 1-5" do
+        log_fail = FactoryGirl.build(:log, rating: 6)
+        expect(log_fail).to_not be_valid
+        log_pass = FactoryGirl.build(:log, rating: 5)
+        expect(log_pass).to be_valid
     end
     it "is valid without a comment" do
         log = FactoryGirl.build(:log, comment: nil)
         expect(log).to be_valid
     end
-    it "only allows Text, Audio, Video, In-Person responses for type"
-    it "only allows the rating scale to be a number from 1-5"
 end
