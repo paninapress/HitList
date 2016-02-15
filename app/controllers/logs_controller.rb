1653b4543bf565268845e748a376504c88f7a05c
paninapress <npacifico25@gmail.com>
2016-02-15 13:27:10 -0800

    can create logs!

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,5 +3,9 @@ class LogsController < ApplicationController
 		@friend = Friend.find(params[:friend_id])
 	end
 	def create
+		log = Log.new(params.require(:log).permit(:date, :type_of, :rating, :comment))
+		log.update_attributes(friend_id: params[:friend_id])
+		log.save!
+		redirect_to friend_path(log.friend_id)
 	end
 end
--- spec/controllers/logs_controller_spec.rb
+++ spec/controllers/logs_controller_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe LogsController, type: :controller do
             expect(response).to have_http_status(200)
         end
     end
-    # context "POST #create" do
-    #     login_user
-    #     it "creates new log" do
-    #         expect{ post :create, log: FactoryGirl.attributes_for(:log) }.to change(Log,:count).by(1)
-    #     end
-    # end
+    context "POST #create" do
+        login_user
+        it "creates new log" do
+            expect{ post :create, {friend_id: Friend.last, log: FactoryGirl.attributes_for(:log)} }.to change(Log,:count).by(1)
+        end
+    end
 end
