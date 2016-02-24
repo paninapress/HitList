ce8f1149d0cb109cee078e2f4fecdefe78af0dd0
paninapress <npacifico25@gmail.com>
2016-02-23 18:10:07 -0800

    updated views to display/edit/delete logs

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
   root 'site#index'
-  delete '/friends/:id', to: 'friends#destroy', as: 'remove_friend'
   resources :friends do
     resources :logs
   end
 end
-
-#                   Prefix Verb   URI Pattern                       Controller#Action
-#         new_user_session GET    /users/sign_in(.:format)          users/sessions#new
-#             user_session POST   /users/sign_in(.:format)          users/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)         users/sessions#destroy
-#            user_password POST   /users/password(.:format)         devise/passwords#create
-#        new_user_password GET    /users/password/new(.:format)     devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format)    devise/passwords#edit
-#                          PATCH  /users/password(.:format)         devise/passwords#update
-#                          PUT    /users/password(.:format)         devise/passwords#update
-# cancel_user_registration GET    /users/cancel(.:format)           users/registrations#cancel
-#        user_registration POST   /users(.:format)                  users/registrations#create
-#    new_user_registration GET    /users/sign_up(.:format)          users/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)             users/registrations#edit
-#                          PATCH  /users(.:format)                  users/registrations#update
-#                          PUT    /users(.:format)                  users/registrations#update
-#                          DELETE /users(.:format)                  users/registrations#destroy
-#        user_confirmation POST   /users/confirmation(.:format)     devise/confirmations#create
-#    new_user_confirmation GET    /users/confirmation/new(.:format) devise/confirmations#new
-#                          GET    /users/confirmation(.:format)     devise/confirmations#show
-#                     root GET    /                                 site#index
