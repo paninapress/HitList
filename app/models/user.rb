0feb85f515093ef927ba8131b4238dc80663233b
paninapress <npacifico25@gmail.com>
2016-02-26 07:09:30 -0800

    refactoring user spec and added dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  has_many :friends
+  has_many :friends, dependent: :destroy
   validates :username, presence: true
   validates :username, length: {maximum: 50}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,28 +5,42 @@ describe User, :type => :model do
         user = FactoryGirl.build(:user)
         expect(user).to be_valid
     end
-    it "must have a username" do
-        user = FactoryGirl.build(:user, username: nil)
-        expect(user).to_not be_valid
+    context "is invalid" do
+        subject(:user){ FactoryGirl.build(:user) }
+
+        it "without a :username" do
+            user[:username] = nil
+            expect(user).to_not be_valid
+        end
+        it "if :username > 50 characters" do
+            user[:username] = "abcde"*12
+            expect(user).to_not be_valid
+        end
+        it "without an :email" do
+            user[:email] = nil
+            expect(user).to_not be_valid
+        end
+        it "without a :password" do
+            user = FactoryGirl.build(:user, password: nil) #intentionally left like this b/c "can't write unknown attribute `password`" error
+            expect(user).to_not be_valid
+        end
+        it "without a unique :email" do
+            user = FactoryGirl.create(:user, email: "test@test.com")
+            expect(FactoryGirl.build(:user, email: "test@test.com")).to_not be_valid
+        end
     end
-    it "must have a username with less than 50 char" do
-        user=FactoryGirl.build(:user,username:"abcde"*12)
-        expect(user).to_not be_valid
-    end
-    it "must have an email" do
-    	user = FactoryGirl.build(:user, email: nil)
-    	expect(user).to_not be_valid
-    end
-    it "must have a password" do
-    	user = FactoryGirl.build(:user, password: nil)
-    	expect(user).to_not be_valid
-    end
-    it "must have a unique email" do
-    	user1 = FactoryGirl.create(:user, email: "test@test.com")
-    	expect(FactoryGirl.build(:user, email: "test@test.com")).to_not be_valid
-    end
-    it "has many friends" do
-        user = FactoryGirl.create(:user)
-        expect(user.friends.count).to be > 0
+    context "with friends" do
+        let(:user){ FactoryGirl.create(:user) }
+        let(:friend){ FactoryGirl.create(:friend, user_id: user.id) }
+        before(:each) do
+            expect(friend).to be_valid
+        end
+
+        it "can have many friends" do
+            expect(user.friends.count).to be > 0
+        end
+        it "has dependent destroy for user.friends" do
+            expect{user.destroy}.to change{user.friends.count}.from(1).to(0)
+        end
     end
 end
