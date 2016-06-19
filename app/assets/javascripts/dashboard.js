11aa0eec4457d4f798a18dfd911d5f09dd7148a2
paninapress <npacifico25@gmail.com>
2016-06-19 00:30:53 -0700

    added nav active functionality and fixed turbolinks issue

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -20,11 +20,21 @@ angular.module('DashboardApp').controller(
 			if(viewThis=='list-view'){
 				$('.stars-view').addClass('hide');
 				$('.list-view').removeClass('hide');
+				console.log('yeps list');
 			}
 			else if(viewThis=='stars-view'){
 				$('.list-view').addClass('hide');
 				$('.stars-view').removeClass('hide');
+				console.log('ok stars');
+			}
+			else{
+				console.log('nope');
 			}
 		}
+		// nav active
+		$(".nav a").on("click", function(){
+   		$(".nav").find(".active").removeClass("active");
+   		$(this).parent().addClass("active");
+});
 	}
 ]);
