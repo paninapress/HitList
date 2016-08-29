6e3c8d81991eb691e8e539bd39026fee104def51
paninapress <npacifico25@gmail.com>
2016-08-28 22:33:50 -0700

    changed for devise testhelper depreciating

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,5 +1,5 @@
 require 'support/controller_macros'
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::Test::ControllerHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
 end
